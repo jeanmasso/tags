14bd54a27d0c0419768fbec208ce3c4a040a75e7
jean <jmasso@xlprog.nc>
2020-12-29 13:33:32 +1100

    maj tags

--- script.js
+++ script.js
@@ -1,16 +1,35 @@
 function searchTag() {
-    var searchbar, filter, tagList, li, span, i, txtValue;
+    var searchbar, filter, tagList, div, a, i, txtValue;
     searchbar = document.getElementById("searchbar");
     filter = searchbar.value.toUpperCase();
-    tagList = document.getElementById("tagList");
-    li = tagList.getElementsByTagName("li");
-    for (i = 0; i < li.length; i++) {
-        span = li[i].getElementsByTagName("span")[0];
-        txtValue = span.textContent || span.innerText;
+    tagList = document.getElementById("searchTagList");
+    div = tagList.getElementsByTagName("div");
+    for (i = 0; i < div.length; i++) {
+        a = div[i].getElementsByTagName("a")[0];
+        txtValue = a.textContent || a.innerText;
         if (txtValue.toUpperCase().indexOf(filter) > -1) {
-            li[i].style.display = "";
+            div[i].style.display = "";
         } else {
-            li[i].style.display = "none";
+            div[i].style.display = "none";
         }
     }
 }
+
+function createTag() {
+    var newTag = $("#newTag").val();
+    var newTagColor = $("#newTagColor").val();
+    if (newTag === "") return ''; else {
+        $("#searchTagList").append(`
+            <div class="tag p-1"><a href="#" class="dropdown-item badge m-0" style="background-color: ${newTagColor}; color: white;">${newTag} <i class="fas fa-times"></i></a></li>
+        `);
+        $("#modalTagList").append(`
+            <div class="tag p-1"><a href="#" class="dropdown-item badge m-0" style="background-color: ${newTagColor}; color: white;">${newTag} <i class="fas fa-times"></i></a></div>
+        `);
+        $("#newTag").val("");
+    };
+}
+
+$(document).on("click", ".badge i", function(){
+    $(".badge").parent(".tag").remove();
+});
+
